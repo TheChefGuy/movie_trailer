0b78024ec40e8d7099d96274ab1a037b07b17aab
SoonJooUh <madeforonebox@gmail.com>
2017-08-11 10:47:59 -0700

    movie posters and previews

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,16 +1,39 @@
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie("Toy Story",
                         "A story about a boy and his toys that come to life",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "http://www.youtube.com/watch?v=vwyzh85nqc4")
-print(toy_story.storyline)
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+#print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
                         "A marine on an alien planet",
-                        "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg,jpg",
+                        "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
                         "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-print(avatar.storyline)
-avatar.show_trailer()
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+school_of_rock = media.Movie("School of Rock",
+                        "Usig rock music to learn",
+                        "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                        "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille",
+                        "A rat is a chef in Paris",
+                        "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                        "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+chef = media.Movie("Chef",
+                "A chef and his food truck",
+                "https://upload.wikimedia.org/wikipedia/en/b/b8/Chef_2014.jpg",
+                "https://www.youtube.com/watch?v=qK-ZUFX5fnk")
+
+hunger_games = media.Movie("Hunger_Games",
+                        "A really real reality show",
+                        "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                        "https://www.youtube.com/watch?v=PbA63a7H0bo")
 
 
+movies = [toy_story, avatar, school_of_rock, ratatouille, chef, hunger_games]
+fresh_tomatoes.open_movies_page(movies)
